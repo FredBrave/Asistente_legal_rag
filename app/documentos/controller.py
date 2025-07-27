f4fc8c6358d72dda80c00a75f0f3ff09b3b897b0
FredBrave <freddy.bravo.robalino@gmail.com>
2025-07-27 17:53:00 -0500

    Docs about subir and listar

--- app/documentos/controller.py
+++ app/documentos/controller.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from app.documentos.service import guardar_documento, listar_documentos, procesar_documento
 
-router = APIRouter(prefix="/documentos", tags=["Documentos"])
+router = APIRouter(prefix="/documentos", tags=["Gestion de Documentos"])
 
-@router.post("/subir")
+@router.post("/subir", summary="Sube un documento legal", description="Permite subir archivos .pdf o .txt y una vez subidos son indexados")
 async def subir_documento(file: UploadFile = File(...)):
     if not file.filename.endswith((".pdf", ".txt")):
         raise HTTPException(status_code=400, detail="Formato no soportado")
@@ -11,6 +11,6 @@ async def subir_documento(file: UploadFile = File(...)):
     procesar_documento(path)
     return {"mensaje": f"Documento {file.filename} cargado y procesado."}
 
-@router.get("/listar")
+@router.get("/listar", summary="Lista todos los documentos guardados", description="Lista todos los documentos subidos dentro del servidor, los cuales pueden ser ")
 def ver_documentos():
     return listar_documentos()
